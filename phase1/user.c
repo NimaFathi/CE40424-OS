87d2c11eb416650ba6877e2e78754d3be9894c24
Niwgoat <nimafathinimafathi@gmail.com>
2020-05-18 04:17:44 +0430

    update user.c

--- phase1/user.c
+++ phase1/user.c
@@ -1,8 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+static const char pcb_dev[] = "/dev/pcb";
 
 int main(int argc, char const *argv[]) {
+  long read_out ;
+  int sz;
+  char* buf = (char *) calloc(100, sizeof(char));
   int pid = 0;
   int period = 0;
   if (argc < 5) {
@@ -18,14 +26,37 @@ int main(int argc, char const *argv[]) {
         period = atoi(argv[i+1]);
       }
       else if (strcmp(argv[i], "--pid") == 0) {
-        pid = atoi(argv[i+1]);
+        pid = (pid_t) atoi(argv[i+1]);
       } else {
         printf("unavailable argument\n" );
         return 0;
       }
     }
-    printf("%d\n", pid);
-    printf("%d\n", period);
+    printf("%d\n",pid);
+      printf("%d\n", period);
   }
+  int fd = open(pcb_dev, O_RDWR );
+  if (fd < 0) {
+    perror("faild to open character device\n");
+    exit(1);
+  }
+  // convert int to string
+  int length = snprintf(NULL, 0 , "%d", pid);
+  char * str = malloc(length + 1);
+  snprintf(str, length + 1 , "%d", pid);
+  printf("%s\n",str );
+
+  // write pid into driver file (working)
+  lseek(fd, 0, SEEK_SET);
+  sz = write(fd, str,strlen(str));
+  printf("%d\n",sz);
+  free(str);
+  close(fd);
+  // BUG: read from driver(not working) -> there must be a bug in "pcb_device_file_read" function
+  fd = open(pcb_dev, O_RDWR );
+  read_out = read(fd , buf,10);
+  printf("%ld\n", read_out); //print number of read bytes
+  buf[read_out] = '\0';
+  printf("%s\n", buf ); // print desired output
   return 0;
 }
